a14db14552339e6470ad85954c1c21c16a6d189b
Eucer <100469001+Eucer@users.noreply.github.com>
2022-07-12 16:17:49 -0400

    co

--- lib/features/orderDetails/screens/orderDetails.dart
+++ lib/features/orderDetails/screens/orderDetails.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:v1douvery/NAV/centerSearchNav.dart';
@@ -54,84 +55,22 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text(
-                        'View order details',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black12,
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color(0xff05595B)),
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'View order details',
+                            style: TextStyle(
+                                fontSize: 17,
+                                color: GlobalVariables.colorTextWhiteLight),
                           ),
                         ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Order ID:          ${widget.order.id}'),
-                            Text(
-                                'Order Total:      \$${widget.order.totalPrice}'),
-                          ],
-                        ),
                       ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        'Purchase Details',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.black12,
-                            ),
-                            color: GlobalVariables.backgroundColor),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            for (int i = 0;
-                                i < widget.order.products.length;
-                                i++)
-                              Row(
-                                children: [
-                                  Image.network(
-                                    widget.order.products[i].images[0],
-                                    height: 120,
-                                    width: 120,
-                                  ),
-                                  const SizedBox(width: 5),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          widget.order.products[i].name,
-                                          style: const TextStyle(
-                                            fontSize: 17,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                          maxLines: 2,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                        Text(
-                                          'Qty: ${widget.order.quantity[i]}',
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 10),
                       const Text(
                         'Tracking',
                         style: TextStyle(
@@ -140,11 +79,6 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                         ),
                       ),
                       Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black12,
-                          ),
-                        ),
                         child: Stepper(
                           currentStep: currentStep,
                           controlsBuilder: (context, details) {
@@ -159,8 +93,26 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                           steps: [
                             Step(
                               title: const Text('Pending'),
-                              content: const Text(
-                                'Your order is yet to be delivered',
+                              content: Column(
+                                children: [
+                                  Container(
+                                    width: 90,
+                                    height: 80,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.rectangle,
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/images/envioShopping.png'),
+                                        // picked file
+                                        fit: BoxFit.contain,
+                                      ),
+                                      color: GlobalVariables.backgroundColor,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Your order is yet to be delivered',
+                                  ),
+                                ],
                               ),
                               isActive: currentStep > 0,
                               state: currentStep > 0
@@ -200,6 +152,88 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                           ],
                         ),
                       ),
+                      const Text(
+                        'Product',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black12,
+                            ),
+                            color: GlobalVariables.backgroundColor),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            for (int i = 0;
+                                i < widget.order.products.length;
+                                i++)
+                              Row(
+                                children: [
+                                  CachedNetworkImage(
+                                    imageUrl:
+                                        widget.order.products[i].images[0],
+                                    height: 120,
+                                    width: 120,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          widget.order.products[i].name,
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(
+                                          '${widget.order.products[i].marca}',
+                                        ),
+                                        Text(
+                                          'Qty: ${widget.order.quantity[i]}',
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black12,
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Order ID:          ${widget.order.id}'),
+                            Text(
+                                'Order Total:      \$${widget.order.totalPrice}'),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Purchase Details',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
                     ],
                   ),
                 ),
