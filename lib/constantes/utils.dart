9f3563f59bc1a37676090e08fb4168dea89b174a
Eucer <100469001+Eucer@users.noreply.github.com>
2022-07-13 16:56:18 -0400

    ccc

--- lib/constantes/utils.dart
+++ lib/constantes/utils.dart
@@ -4,10 +4,8 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(text),
-    ),
+  SnackBar(
+    content: Text(text),
   );
 }
 
