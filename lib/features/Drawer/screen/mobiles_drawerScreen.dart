e19e758073901f1557bcb169d7f26b2586e58270
Eucer <100469001+Eucer@users.noreply.github.com>
2022-09-23 12:32:32 -0400

    cc

--- lib/features/Drawer/screen/mobiles_drawerScreen.dart
+++ lib/features/Drawer/screen/mobiles_drawerScreen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/cli_commands.dart';
 import 'package:provider/provider.dart';
+import 'package:v1douvery/common/widgets/IconButton.dart';
 
+import '../../../common/widgets/bottom_barAdmin.dart';
 import '../../../constantes/global_variables.dart';
 import '../../../models/themePreference.dart';
 import '../../../provider/theme.dart';
@@ -28,10 +30,40 @@ class DrawerScreen extends StatelessWidget {
               : GlobalVariables.backgroundColor,
           child: Column(
             children: [
-              headerWidget(),
+              Provider.of<UserProvider>(context).user.token.isNotEmpty
+                  ? Provider.of<UserProvider>(context).user.type == 'user'
+                      ? headerWidget()
+                      : HeaderWidgetAdmin()
+                  : Container(
+                      child: Text(
+                        'No registrado',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: currentTheme.isDarkTheme()
+                              ? GlobalVariables.text1darkbackgroundColor
+                              : GlobalVariables.text1WhithegroundColor,
+                        ),
+                      ),
+                    ),
               const SizedBox(
                 height: 20,
               ),
+              Provider.of<UserProvider>(context).user.type == 'admin'
+                  ? DrawerItem(
+                      name: 'Panel de admin',
+                      icon: Icons.admin_panel_settings,
+                      onPressed: () {
+                        Navigator.of(context, rootNavigator: true).push(
+                          // ensures fullscreen
+                          MaterialPageRoute(
+                            builder: (context) => MainScreenAdmin(),
+                          ),
+                        );
+                      },
+                    )
+                  : const SizedBox(
+                      height: 0,
+                    ),
               Divider(
                 thickness: 1,
                 height: 10,
@@ -174,6 +206,65 @@ class headerWidget extends StatelessWidget {
             Text(user.type.toUpperCase(),
                 style: TextStyle(
                   fontSize: 12,
+                  color: currentTheme.isDarkTheme()
+                      ? GlobalVariables.text1darkbackgroundColor
+                      : GlobalVariables.text1WhithegroundColor,
+                ))
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class HeaderWidgetAdmin extends StatelessWidget {
+  const HeaderWidgetAdmin({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const url =
+        'https://media.istockphoto.com/photos/learn-to-love-yourself-first-picture-id1291208214?b=1&k=20&m=1291208214&s=170667a&w=0&h=sAq9SonSuefj3d4WKy4KzJvUiLERXge9VgZO-oqKUOo=';
+    final currentTheme = Provider.of<ThemeProvider>(context);
+    final user = Provider.of<UserProvider>(context).user;
+    return Row(
+      children: [
+        const CircleAvatar(
+          radius: 40,
+          backgroundImage: NetworkImage(url),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  user.name.capitalize(),
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: currentTheme.isDarkTheme()
+                        ? GlobalVariables.text1darkbackgroundColor
+                        : GlobalVariables.text1WhithegroundColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(user.type.toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: currentTheme.isDarkTheme()
+                          ? GlobalVariables.text1darkbackgroundColor
+                          : GlobalVariables.text1WhithegroundColor,
+                    ))
+              ],
+            ),
+            CustomnIconsButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.admin_panel_settings,
                   color: currentTheme.isDarkTheme()
                       ? GlobalVariables.text1darkbackgroundColor
                       : GlobalVariables.text1WhithegroundColor,
