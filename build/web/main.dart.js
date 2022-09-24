43ec3a458e2285e7363795f52dfaf611f699ce06
Eucer <100469001+Eucer@users.noreply.github.com>
2022-09-24 10:04:53 -0400

    cccc

--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -108793,3 +108793,4 @@ a(b.target)}for(var r=0;r<s.length;++r)s[r].addEventListener("load",onLoad,false
 var s=A.aPP
 if(typeof dartMainRunner==="function")dartMainRunner(s,[])
 else s([])})})()
+//# sourceMappingURL=main.dart.js.map
